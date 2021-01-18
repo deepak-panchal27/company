bee2ad928771653f92d2d0584058b20ad317207f
deepak.panchal <Deepak.Panchal@bom.tv18.com>
2021-01-18 17:23:13 +0530

    Added folder structure and schema

---
+++ company.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 18, 2021 at 12:50 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `company`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address_master`
+--
+
+CREATE TABLE `address_master` (
+  `Address_ID` int(11) NOT NULL,
+  `Employee_Master_ID` int(11) NOT NULL,
+  `Address1` varchar(100) NOT NULL,
+  `Address2` varchar(100) NOT NULL,
+  `Address3` varchar(100) NOT NULL,
+  `Created_Date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updated_Date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `Department_ID` int(11) NOT NULL,
+  `Department_Name` varchar(100) NOT NULL,
+  `Created_Date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updated_Date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_master`
+--
+
+CREATE TABLE `employee_master` (
+  `Employee_ID` int(11) NOT NULL,
+  `Department_ID` int(11) NOT NULL,
+  `Employee_Name` varchar(100) NOT NULL,
+  `Created_Date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updated_Date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phone_master`
+--
+
+CREATE TABLE `phone_master` (
+  `Phone_ID` int(11) NOT NULL,
+  `Address_ID` int(11) NOT NULL,
+  `Phone_No` int(10) NOT NULL,
+  `Created_Date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `Updated_Date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address_master`
+--
+ALTER TABLE `address_master`
+  ADD PRIMARY KEY (`Address_ID`),
+  ADD KEY `Employee_Master_ID` (`Employee_Master_ID`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`Department_ID`);
+
+--
+-- Indexes for table `employee_master`
+--
+ALTER TABLE `employee_master`
+  ADD PRIMARY KEY (`Employee_ID`),
+  ADD KEY `Department_ID` (`Department_ID`);
+
+--
+-- Indexes for table `phone_master`
+--
+ALTER TABLE `phone_master`
+  ADD PRIMARY KEY (`Phone_ID`),
+  ADD KEY `Address_ID` (`Address_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `address_master`
+--
+ALTER TABLE `address_master`
+  MODIFY `Address_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `Department_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee_master`
+--
+ALTER TABLE `employee_master`
+  MODIFY `Employee_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `phone_master`
+--
+ALTER TABLE `phone_master`
+  MODIFY `Phone_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `address_master`
+--
+ALTER TABLE `address_master`
+  ADD CONSTRAINT `address_master_ibfk_1` FOREIGN KEY (`Employee_Master_ID`) REFERENCES `employee_master` (`Employee_ID`);
+
+--
+-- Constraints for table `employee_master`
+--
+ALTER TABLE `employee_master`
+  ADD CONSTRAINT `employee_master_ibfk_1` FOREIGN KEY (`Department_ID`) REFERENCES `department` (`Department_ID`);
+
+--
+-- Constraints for table `phone_master`
+--
+ALTER TABLE `phone_master`
+  ADD CONSTRAINT `phone_master_ibfk_1` FOREIGN KEY (`Address_ID`) REFERENCES `address_master` (`Address_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
